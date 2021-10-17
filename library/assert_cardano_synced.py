ad8dc8232cc854c28d50e841d8fc8971e7714772
Grzegorz Nowak <strange3studio@gmail.com>
2021-10-17 15:56:02 +0200

    complete the assertion module with basic integration test

--- library/assert_cardano_synced.py
+++ library/assert_cardano_synced.py
@@ -2,35 +2,34 @@ from ansible.module_utils.basic import AnsibleModule
 
 import os
 import json
-import subprocess
-
-from library.cardano_wallet import (
-    collect_wallets,
-    BrokenWalletsError,
-)
 
 
-def query_tip(active_network, testnet_magic, cardano_bin_path):
+def query_tip_cmd(cardano_node_socket, active_network, testnet_magic, cardano_bin_path):
 
     raw_utxo_table = None
+    command = ""
     if active_network == 'test':
-        raw_response = subprocess.check_output(
-            [cardano_bin_path, 'query', 'tip', '--testnet-magic', testnet_magic])
+        command = "CARDANO_NODE_SOCKET_PATH={0} " \
+                  "{1}/cardano-cli query tip " \
+                  "--testnet-magic {2} ".format(cardano_node_socket,
+                                                cardano_bin_path,
+                                                testnet_magic)
     elif active_network == 'main':
-        raw_response = subprocess.check_output(
-            [cardano_bin_path, 'query', 'utxo', '--mainnet'])
+        command = "CARDANO_NODE_SOCKET_PATH={0} " \
+                  "{1}/cardano-cli query tip --mainnet".format(cardano_node_socket,
+                                                               cardano_bin_path)
 
-    return raw_response
+    return command
 
 
 def get_progress_from_response(raw_response):
     return float(json.loads(raw_response)['syncProgress'])
 
 
-
 def main():
 
     argument_spec = dict(
+        cardano_node_socket=dict(type='str', required=True),
         wanted_progress=dict(type='float', default='100'),
         cardano_bin_path=dict(type='path', default='~/bin'),
         active_network=dict(type='str', default='test',
@@ -40,21 +39,27 @@ def main():
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=True)
 
+    cardano_node_socket = module.params['cardano_node_socket']
     active_network = module.params['active_network']
     wanted_progress = module.params['wanted_progress']
     testnet_magic = module.params['testnet_magic']
     cardano_bin_path = module.params['cardano_bin_path']
 
-    sync_progress = get_progress_from_response(query_tip(
-        active_network,
-        testnet_magic,
-        cardano_bin_path
-    ))
+    code, tip_responce, stderr = module.run_command(query_tip_cmd(cardano_node_socket,
+                                                                  active_network,
+                                                                  testnet_magic,
+                                                                  cardano_bin_path),
+                                                    use_unsafe_shell=True)
+
+    assert code == 0
+    assert stderr == ""
+
+    sync_progress = float(get_progress_from_response(tip_responce))
 
     if sync_progress >= wanted_progress:
-        module.fail_json(msg="Expected progress not achieved yet", progress=sync_progress)
-    else:
         module.exit_json(changed=False, progress=sync_progress)
+    else:
+        module.fail_json(msg="Expected progress not achieved yet", progress=sync_progress)
 
 if __name__ == '__main__':
     main()
