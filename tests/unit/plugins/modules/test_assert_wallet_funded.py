ea752d8d9a99b7327818ee130fc1c79c255bf1ab
Grzegorz Nowak <strange3studio@gmail.com>
2021-10-18 09:12:08 +0200

    Feature/13 (#17)
    * wallet assertion module added. 
    
    * add unit and integration tests
    
    * test wallet assertions against unsynced node

---
+++ tests/unit/plugins/modules/test_assert_wallet_funded.py
@@ -0,0 +1,36 @@
+import pytest
+
+from library.assert_address_funded import (
+    get_lovelace_from_utxo,
+    create_utxo_command
+)
+
+def test_creating_utxo_command():
+    cmd = create_utxo_command("cardano_node_socket",
+                              "test",
+                              "testnet_magic",
+                              "bin_path",
+                              "addr")
+    print(cmd)
+    assert cmd == "CARDANO_NODE_SOCKET_PATH=cardano_node_socket bin_path/cardano-cli query utxo " \
+                  "--testnet-magic testnet_magic --address addr"
+
+def test_getting_progress_from_response():
+
+    lovelace = 1234567
+
+    sample_response='''
+                               TxHash                                 TxIx        Amount
+--------------------------------------------------------------------------------------
+21a4641241ff937c8bbb14bc4493089c9a9fe1b759195e07d66e506106b76adf     0        %d lovelace + TxOutDatumNone
+    ''' % lovelace
+
+    address_lovelace = get_lovelace_from_utxo(sample_response)
+
+    assert address_lovelace == lovelace
+
+    sample_response2=''''''
+
+    address_lovelace2 = get_lovelace_from_utxo(sample_response2)
+
+    assert address_lovelace2 == 0
