1607606f4929a0c4c413030dcfaa79d24aad8ea2
Grzegorz Nowak <strange3studio@gmail.com>
2021-10-22 18:13:43 +0200

    augment with new meaningful test

--- tests/unit/plugins/modules/test_assert_wallet_funded.py
+++ tests/unit/plugins/modules/test_assert_wallet_funded.py
@@ -5,6 +5,7 @@ from library.assert_address_funded import (
     create_utxo_command
 )
 
+
 def test_creating_utxo_command():
     cmd = create_utxo_command("cardano_node_socket",
                               "test",
@@ -15,7 +16,8 @@ def test_creating_utxo_command():
     assert cmd == "CARDANO_NODE_SOCKET_PATH=cardano_node_socket bin_path/cardano-cli query utxo " \
                   "--testnet-magic testnet_magic --address addr"
 
-def test_getting_progress_from_response():
+
+def test_calculating_funds_from_utxo():
 
     lovelace = 1234567
 
@@ -43,3 +45,15 @@ def test_getting_progress_from_response():
     address_lovelace3 = get_lovelace_from_utxo(sample_response3)
 
     assert address_lovelace3 == 0
+
+    sample_response4 = '''
+                           TxHash                                 TxIx        Amount
+--------------------------------------------------------------------------------------
+21a4641241ff937c8bbb14bc4493089c9a9fe1b759195e07d66e506106b76adf     0        1000000000 lovelace + TxOutDatumNone
+da7bcb251f3a90e74d8d661a55d2e8951f92932d9cdea0172ed70e8f2be47a5e     0        1000000000 lovelace + TxOutDatumNone
+
+    '''
+
+    address_lovelace4 = get_lovelace_from_utxo(sample_response4)
+
+    assert address_lovelace4 == 2000000000
